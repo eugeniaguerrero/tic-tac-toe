6bc8fe17fdb1f6b78cd629daebc41d760acda941
Eugenia Guerrero <eugenia.guerrero@me.com>
2017-05-18 11:37:36 +0100

    allows player 2 to make a move

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -7,8 +7,15 @@ describe Game do
     expect(game.grid).to eq [[nil, nil, nil], [nil, nil, nil], [nil, nil, nil]]
   end
 
-  it 'allows the player to make a move' do
-    game.play(0,0,1)
-    expect(game.grid).to eq [[1, nil, nil], [nil, nil, nil], [nil, nil, nil]]
+  context 'making a move' do
+    it 'allows player 1 to make a move' do
+      game.play(0,0,1)
+      expect(game.grid).to eq [[1, nil, nil], [nil, nil, nil], [nil, nil, nil]]
+    end
+    it'allows player 2 to make a move' do
+      game.play(0,1,2)
+      expect(game.grid).to eq [[nil, 2, nil],[nil, nil, nil], [nil, nil, nil]]
+    end
   end
+
 end
